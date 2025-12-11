de9a8bb58e93f873e938090125561749a62be133
goaload <52301709+goaload@users.noreply.github.com>
2025-12-11 13:49:27 +0800

    Update config.js

--- themes/typography/config.js
+++ themes/typography/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // 博客標題 雙語言
   TYPOGRAPHY_BLOG_NAME: process.env.NEXT_PUBLIC_TYPOGRAPHY_BLOG_NAME || '听听土豆 唱唱反调',
-  TYPOGRAPHY_BLOG_NAME_EN: process.env.NEXT_PUBLIC_TYPOGRAPHY_BLOG_NAME_EN || process.env.NEXT_PUBLIC_TYPOGRAPHY_BLOG_NAME || 'Typography',
+  TYPOGRAPHY_BLOG_NAME_EN: process.env.NEXT_PUBLIC_TYPOGRAPHY_BLOG_NAME_EN || process.env.NEXT_PUBLIC_TYPOGRAPHY_BLOG_NAME || 'I solemnly swear that I am up to no good',
 
   TYPOGRAPHY_POST_AD_ENABLE: process.env.NEXT_PUBLIC_TYPOGRAPHY_POST_AD_ENABLE || false, // 文章列表是否插入广告
 
