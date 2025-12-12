6f8ae25510e36fd65e68a689cc9cc2792d8a1ce9
goaload <52301709+goaload@users.noreply.github.com>
2025-12-12 21:57:53 +0800

    Update contact.config.js

--- conf/contact.config.js
+++ conf/contact.config.js
@@ -1,7 +1,8 @@
 /**
  * 社交按钮相关的配置同意放这
  */
-module。exports = {
+module。exports = {CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || 'https://x.com/GoaLoad', // 你的twitter个人主页
+                  CONTACT_INSTAGRAM: process.env.NEXT_PUBLIC_CONTACT_INSTAGRAM || 'https://www.instagram.com/ipotatooooo/', // 您的instagram地址
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL:
     (process.env.NEXT_PUBLIC_CONTACT_EMAIL &&
